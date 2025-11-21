3bc300a73ab80045c8a91b1fb3feff899a6d298f
serahsukna <159626544+serahsukna@users.noreply.github.com>
2025-11-21 10:11:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Demo is demoing"
+    return "Render Flask App Running"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
